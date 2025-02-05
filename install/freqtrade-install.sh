b6609ff2a96050f880491cf4ec618b716a3e84c6
Slaviša Arežina <arezina.slavisa@gmail.com>
2025-02-05 20:33:39 +0100

    f

--- install/freqtrade-install.sh
+++ install/freqtrade-install.sh
@@ -28,8 +28,8 @@ tar xvzf ta-lib-0.4.0-src.tar.gz
 cd ta-lib
 sed -i.bak "s|0.00000001|0.000000000000000001 |g" src/ta_func/ta_utility.h
 ./configure --prefix=/usr/local
-make
-make install
+make -j 2
+make install -j 2
 ldconfig
 msg_ok "Installed Dependencies"
 
