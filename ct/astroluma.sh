590230d68c8a0bab13ecfe3867fe66ed593fe14c
tremor021 <arezina.slavisa@gmail.com>
2025-02-08 15:51:53 +0100

    asd

--- ct/astroluma.sh
+++ ct/astroluma.sh
@@ -9,7 +9,7 @@ source <(curl -s https://raw.githubusercontent.com/tremor021/ProxmoxVE/refs/head
 APP="Astroluma"
 var_tags="dashboard"
 var_cpu="1"
-var_ram="1024"
+var_ram="2048"
 var_disk="8"
 var_os="debian"
 var_version="12"
