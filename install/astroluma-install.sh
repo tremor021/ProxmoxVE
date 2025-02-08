27cc42ef6f4fb64ef4647cd0cd01af25327a1c8d
tremor021 <arezina.slavisa@gmail.com>
2025-02-08 19:41:02 +0100

    asd

--- install/astroluma-install.sh
+++ install/astroluma-install.sh
@@ -26,23 +26,22 @@ $STD apt-get update
 $STD apt-get install mongodb-org -y
 systemctl enable -q --now mongod
 sleep 2
-# MONGO_ADMIN_USER="admin"
-#MONGO_ADMIN_PWD="$(openssl rand -base64 18 | cut -c1-13)"
-#$STD mongosh <<EOF
-#use admin
-#db.createUser({
-#  user: "$MONGO_ADMIN_USER",
-#  pwd: "$MONGO_ADMIN_PWD",
-#  roles: [{ role: "root", db: "admin" }]
-#})
-#quit()
-#EOF
-#{
-#    echo "MongoDB Credentials"
-#    echo "Mongo Database User: $MONGO_ADMIN_USER"
-#    echo "Mongo Database Password: $MONGO_ADMIN_PWD"
-#} >> ~/astroluma.creds
-#
+MONGO_ADMIN_USER="admin"
+MONGO_ADMIN_PWD="$(openssl rand -base64 18 | cut -c1-13)"
+$STD mongosh <<EOF
+use admin
+db.createUser({
+  user: "$MONGO_ADMIN_USER",
+  pwd: "$MONGO_ADMIN_PWD",
+  roles: [{ role: "root", db: "admin" }]
+})
+quit()
+EOF
+{
+    echo "MongoDB Credentials"
+    echo "Mongo Database User: $MONGO_ADMIN_USER"
+    echo "Mongo Database Password: $MONGO_ADMIN_PWD"
+} >> ~/astroluma.creds
 curl -s -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash &> /dev/null
 source ~/.bashrc
 $STD nvm install node
@@ -61,10 +60,10 @@ npm run build
 cd ../server
 SECRET=$(openssl rand -hex 16)
 {
-    echo "MONGODB_URI=mongodb://localhost:27017/astroluma"
+    echo "MONGODB_URI=mongodb://127.0.0.1:27017/astroluma"
     echo "SECRET_KEY=$SECRET"
 } >> .env
-msg_ok "Done setting up seelf"
+msg_ok "Done setting up Astroluma"
 
 msg_info "Creating Service"
 cat <<EOF >/etc/systemd/system/astroluma.service
@@ -74,10 +73,8 @@ After=network.target
 
 [Service]
 Type=simple
-User=root
-Group=root
 WorkingDirectory=/opt/astroluma/server
-ExecStart=pm2 start /opt/astroluma/server/server.js
+ExecStart=/opt/astroluma/server/npm run server
 Restart=always
 
 [Install]
