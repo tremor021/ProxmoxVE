1097e817129f2fa341d0bb8bec37dec584cadb0a
tremor021 <arezina.slavisa@gmail.com>
2025-02-10 14:58:22 +0100

    asd

--- ct/tasmocompiler.sh
+++ ct/tasmocompiler.sh
@@ -25,7 +25,7 @@ function update_script() {
     header_info
     check_container_storage
     check_container_resources
-    if [[ ! -d /home/tasmota/tasmocompiler ]]; then
+    if [[ ! -d /opt/tasmocompiler ]]; then
         msg_error "No ${APP} Installation Found!"
         exit
     fi
--- install/tasmocompiler-install.sh
+++ install/tasmocompiler-install.sh
@@ -22,7 +22,7 @@ $STD apt-get install -y \
   gnupg \
   git
 curl -fsSL -o get-platformio.py https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py
-$STD python3 get-platformio.py &> /dev/null
+python3 get-platformio.py &> /dev/null
 msg_ok "Installed Dependencies"
 
 msg_info "Setup Node.js & yarn"
