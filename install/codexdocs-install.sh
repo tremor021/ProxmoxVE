d7037c141698b82a610136d9c5e179cae08c90e7
tremor021 <arezina.slavisa@gmail.com>
2025-02-10 23:38:28 +0100

    asd

--- install/codexdocs-install.sh
+++ install/codexdocs-install.sh
@@ -24,7 +24,7 @@ msg_ok "Installed Dependencies"
 msg_info "Installing Node.js"
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_19.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_18.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
 $STD apt-get update
 $STD apt-get install -y nodejs
 $STD npm install -g yarn
