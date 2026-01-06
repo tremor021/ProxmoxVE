6754b4f58e8a2c0c5d4382d6dc82161029e06493
tremor021 <arezina.slavisa@gmail.com>
2026-01-06 13:58:58 +0100

    Fix

--- tools/pve/pbs_microcode.sh
+++ tools/pve/pbs_microcode.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-# Copyright (c) 2021-2026 tteck
-# Copyright (c) 2026 DonPablo1010
+# Copyright (c) 2026 Community Scripts ORG
 # Adapted for the Proxmox Backup Server - Baremetal Only
 # License: MIT
 # This script searches for CPU microcode packages (Intel/AMD) and offers the option to install them.
