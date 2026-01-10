40ccaf6b7262adf987a6c3e299cdec7e51ea4c6d
tremor021 <arezina.slavisa@gmail.com>
2026-01-11 00:49:54 +0100

    Refactor

--- install/infisical-install.sh
+++ install/infisical-install.sh
@@ -16,11 +16,11 @@ update_os
 msg_info "Installing Dependencies"
 $STD apt install -y \
   apt-transport-https \
-  ca-certificates \
   redis
 msg_ok "Installed Dependencies"
 
 PG_VERSION="17" setup_postgresql
+PG_DB_NAME="infisical_db" PG_DB_USER="infisical" setup_postgresql_db
 
 msg_info "Setting up Infisical Repository"
 setup_deb822_repo \
@@ -29,23 +29,6 @@ setup_deb822_repo \
   "https://artifacts-infisical-core.infisical.com/deb" \
   "stable"
 msg_ok "Setup Infisical repository"
-
-msg_info "Configuring PostgreSQL"
-DB_NAME="infisical_db"
-DB_USER="infisical"
-DB_PASS="$(openssl rand -base64 18 | tr -dc 'a-zA-Z0-9' | cut -c1-13)"
-$STD sudo -u postgres psql -c "CREATE ROLE $DB_USER WITH LOGIN PASSWORD '$DB_PASS';"
-$STD sudo -u postgres psql -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER ENCODING 'UTF8' TEMPLATE template0;"
-$STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET client_encoding TO 'utf8';"
-$STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET default_transaction_isolation TO 'read committed';"
-$STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET timezone TO 'UTC';"
-{
-  echo "Infiscal Credentials"
-  echo "Database Name: $DB_NAME"
-  echo "Database User: $DB_USER"
-  echo "Database Password: $DB_PASS"
-} >>~/infisical.creds
-msg_ok "Configured PostgreSQL"
 
 msg_info "Setting up Infisical"
 AUTH_SECRET="$(openssl rand -base64 32 | tr -d '\n')"
