62cd1429ecdaa4a0e934718e6793731a30c407a7
Chris <punk.sand7393@fastmail.com>
2026-02-06 02:00:39 -0500

    [FIX] Homepage: preserve public/images & public/icons if they exist (#11594)

--- ct/homepage.sh
+++ ct/homepage.sh
@@ -28,7 +28,7 @@ function update_script() {
     exit
   fi
 
-  get_lxc_ip  
+  get_lxc_ip
   NODE_VERSION="22" NODE_MODULE="pnpm@latest" setup_nodejs
   ensure_dependencies jq
 
@@ -39,6 +39,8 @@ function update_script() {
 
     cp /opt/homepage/.env /opt/homepage.env
     cp -r /opt/homepage/config /opt/homepage_config_backup
+    [[ -d /opt/homepage/public/images ]] && cp -r /opt/homepage/public/images /opt/homepage_images_backup
+    [[ -d /opt/homepage/public/icons ]] && cp -r /opt/homepage/public/icons /opt/homepage_icons_backup
     CLEAN_INSTALL=1 fetch_and_deploy_gh_release "homepage" "gethomepage/homepage" "tarball"
     mv /opt/homepage.env /opt/homepage
     rm -rf /opt/homepage/config
@@ -54,6 +56,8 @@ function update_script() {
     export NEXT_PUBLIC_BUILDTIME=$(curl -fsSL https://api.github.com/repos/gethomepage/homepage/releases/latest | jq -r '.published_at')
     export NEXT_TELEMETRY_DISABLED=1
     $STD pnpm build
+    [[ -d /opt/homepage_images_backup ]] && mv /opt/homepage_images_backup /opt/homepage/public/images
+    [[ -d /opt/homepage_icons_backup ]] && mv /opt/homepage_icons_backup /opt/homepage/public/icons
     msg_ok "Updated Homepage"
 
     msg_info "Starting service"
