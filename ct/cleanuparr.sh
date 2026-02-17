43a0a078f5f9fea4ffc99f627eba0dcfb12b51d5
Chris <punk.sand7393@fastmail.com>
2026-02-17 14:17:22 -0500

    [Hotfix] Cleanuparr: backup config before update (#12039)

--- ct/cleanuparr.sh
+++ ct/cleanuparr.sh
@@ -32,7 +32,16 @@ function update_script() {
     systemctl stop cleanuparr
     msg_ok "Stopped Service"
 
+    msg_info "Backing up config"
+    cp -r /opt/cleanuparr/config /opt/cleanuparr_config_backup
+    msg_ok "Backed up config"
+
     CLEAN_INSTALL=1 fetch_and_deploy_gh_release "Cleanuparr" "Cleanuparr/Cleanuparr" "prebuild" "latest" "/opt/cleanuparr" "*linux-amd64.zip"
+
+    msg_info "Restoring config"
+    [[ -d /opt/cleanuparr/config ]] && rm -rf /opt/cleanuparr/config
+    mv /opt/cleanuparr_config_backup /opt/cleanuparr/config
+    msg_ok "Restored config"
 
     msg_info "Starting Service"
     systemctl start cleanuparr
