cf5db6b687e1ab630951e02ad1807ae694fda1bd
SystemIdleProcess <SystemIdleProcess@users.noreply.github.com>
2026-05-23 04:12:59 -0400

    Tunarr: fix path to backup during update (#14655)
    * increase default ram due to oom-killer logs during library scans and fix backup path checking
    
    * fix ram value
    
    * revert resource changes
    
    * correct disk size

--- ct/tunarr.sh
+++ ct/tunarr.sh
@@ -34,11 +34,11 @@ function update_script() {
     msg_ok "Stopped Service"
 
     msg_info "Creating Backup"
-    if [ -d "/usr/local/share/tunarr" ]; then
-      tar -czf "/opt/${APP}_backup_$(date +%F).tar.gz" /usr/local/share/tunarr $STD
+    if [ -d "/root/.local/share/tunarr" ]; then
+      tar -czf "/opt/${APP}_backup_$(date +%F).tar.gz" /root/.local/share/tunarr $STD
       msg_ok "Backup Created"
     else
-      msg_error "Backup failed: /usr/local/share/tunarr does not exist"
+      msg_error "Backup failed: /root/.local/share/tunarr does not exist"
     fi
 
     CLEAN_INSTALL=1 fetch_and_deploy_gh_release "tunarr" "chrisbenincasa/tunarr" "prebuild" "latest" "/opt/tunarr" "*linux-x64.tar.gz"
