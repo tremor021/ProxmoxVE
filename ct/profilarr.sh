a36b2c9be79bd8a75ad0ad17b7ab872fe09c62af
Michael Peleshenko <mpeleshenko@gmail.com>
2026-05-26 03:20:21 -0400

    fix(profilarr): Fix ARCH assignment in profilarr.sh to support Profilarr build usage (#14709)

--- ct/profilarr.sh
+++ ct/profilarr.sh
@@ -37,8 +37,9 @@ function update_script() {
     exit
   fi
 
+  ARCH=$(uname -m)
+
   if check_for_gh_release "deno" "denoland/deno" "v2.7.5" "Deno is pinned to 2.7.5 because the known WouldBlock: Resource temporarily unavailable (os error 11) Issue"; then
-    ARCH=$(uname -m)
     fetch_and_deploy_gh_release "deno" "denoland/deno" "v2.7.5" "latest" "/usr/local/bin" "deno-${ARCH}-unknown-linux-gnu.zip"
   fi
 
